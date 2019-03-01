1de0c1dd49b55089c18b86219ff4c81522a807dc
cristopherahsay <cristopher.dino@ahsay.com>
2019-03-01 09:29:57 +0800

    Update

--- src/windows/winZipExtract.js
+++ src/windows/winZipExtract.js
@@ -1,13 +1,13 @@
-cordova.commandProxy.add("extractFile",{
-    uwp: async function (successCallback, errorCallback, zipFileName) {
-
-        try{
-            var service = new WinRuntimes.Service();  
-            await service.hello(zipFileName);
-            successCallback(1);
-        } catch (e){
-            errorCallback(-1);
+cordova.commandProxy.add("extractFile",
+    {
+        uwp: async function (successCallback,errorCallback,zipFileName){
+            try{
+                var service = new WinRuntimes.Service();  
+                await service.hello(zipFileName);
+                successCallback(1);
+            } catch (e){
+                errorCallback(-1);
+            }
         }
-            
     }
-});
+);
